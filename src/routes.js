bee14fb9c4d7c22ebb635fba8208902ae043b07b
srph <seraphipod@gmail.com>
2016-06-04 05:13:55 +0800

    Add search page

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {Route, IndexRoute} from 'react-router';
 import MainView from './views/main';
 import AppView from './views/app';
 import AppHomeView from './views/app.home';
+import AppSearchView from './views/app.search';
 import LoginView from './views/login';
 import RegistrationView from './views/registration';
 
@@ -10,6 +11,7 @@ export default (
   <Route path="/" component={MainView}>
     <Route component={AppView}>
       <IndexRoute component={AppHomeView} />
+      <Route path="search" component={AppSearchView} />
     </Route>
 
     <Route path="login" component={LoginView} />
