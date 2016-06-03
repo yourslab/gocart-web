8e13942e60ca46f910cbbea36ca0fe024d29d16a
srph <seraphipod@gmail.com>
2016-06-03 22:21:48 +0800

    Add newsfeed

---
+++ src/components/ProductCard/index.jsx
@@ -0,0 +1,86 @@
+import React, {PropType} from 'react';
+import StaticImg from 'app/components/StaticImg';
+
+const ProductCard = () =>
+  <div className="ProductCard">
+    <div className="ProductCard-thumbnail">
+      <img src="https://placeimg.com/500/280/tech" className="ProductCard-thumbnailImage" />
+
+      <div className="ProductCard-thumbnailOverlay">
+        <h1 className="ProductCard-pricing">
+          P 0.00
+        </h1>
+      </div>
+    </div>
+
+    <div className="ProductCard-userPanel">
+      <div className="ProductCard-userPanelSection">
+        <img src="https://placeimg.com/48/48/people" className="ProductCard-avatar" />
+
+        <div className="ProductCard-userPanelMeta">
+          <h4 className="Productcard-name">Mike Greene</h4>
+        </div>
+      </div>
+
+      <div className="ProductCard-userPanelSection">
+      </div>
+    </div>
+
+    <div className="ProductCard-body">
+      <div className="ProductCard-heading">
+        <div>
+          <h4 className="ProductCard-title">Looking for</h4>
+        </div>
+
+        <div className="ProductCard-headingSection">
+          <div className="ProductCard-headingSectionItem">
+            <span className="ProductCard-meta">2 months</span>
+          </div>
+
+          <div className="ProductCard-headingSectionItem">
+            <span className="ProductCard-meta">0.5km</span>
+          </div>
+        </div>
+      </div>
+
+      <p className="ProductCard-description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt  ut labore et dolore magna aliqua.
+      </p>
+
+      <div className="ProductCard-tags">
+        <a href="#" className="TagBadge">Tags</a>
+      </div>
+
+      <div className="CommentMeta">
+        <div>
+          <a href="#" className="CommentMeta-highlight">Mary Ann Smithson</a>&nbsp;
+          and <a href="#" className="CommentMeta-highlight">20 bumped this</a>
+        </div>
+
+        <div>
+          <span className="CommentMeta-count">3 comments</span>
+        </div>
+      </div>
+    </div>
+
+    <div className="ProductCard-actions">
+      <div>
+        <button className="Btn Btn--default Btn--borderless">
+          GoBump
+        </button>
+
+        <button className="Btn Btn--default Btn--borderless">
+          Comment
+        </button>
+      </div>
+
+      <div>
+        <button className="Btn Btn--primary">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  </div>
+
+export default ProductCard;
--- src/views/app.home/index.jsx
+++ src/views/app.home/index.jsx
@@ -1,13 +1,20 @@
 import React, {PropTypes} from 'react';
 import Helmet from 'react-helmet';
+import ProductCard from 'app/components/ProductCard';
 
 class AppHomeView extends React.Component {
   render() {
     return (
-      <div>
+      <div className="Container">
         <Helmet title="Home" />
 
-
+        <div className="Grid">
+          {[null, null, null, null].map(() =>
+            <div className="Grid-cell u-size6 u-spacer">
+              <ProductCard />
+            </div>
+          )}
+        </div>
       </div>
     );
   }
