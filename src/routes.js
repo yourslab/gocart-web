1e2e5e36cb463d078bb4024b7e2d8b604794efe8
srph <seraphipod@gmail.com>
2016-06-06 05:02:34 +0800

    Add profile page

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import MainView from './views/main';
 import AppView from './views/app';
 import AppHomeView from './views/app.home';
 import AppSearchView from './views/app.search';
+import AppProfileView from './views/app.profile';
 import LoginView from './views/login';
 import RegistrationView from './views/registration';
 
@@ -12,6 +13,7 @@ export default (
     <Route component={AppView}>
       <IndexRoute component={AppHomeView} />
       <Route path="search" component={AppSearchView} />
+      <Route path="@:user" component={AppProfileView} />
     </Route>
 
     <Route path="login" component={LoginView} />
