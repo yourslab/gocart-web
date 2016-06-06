03048d8916216d4470326d491c367f213889189a
srph <seraphipod@gmail.com>
2016-06-06 17:58:35 +0800

    Add archive page

--- src/views/app.manage-posts.archived/index.jsx
+++ src/views/app.manage-posts.archived/index.jsx
@@ -1,9 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Helmet from 'react-helmet';
+import ProductCard from 'app/components/ProductCard';
 
 export default class AppManagePostsArchivedView extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <Helmet title="Archived Posts" />
+
+        <div className="Grid">
+          {[[], [], [], [], [], []].map(() =>
+            <div className="Grid-cell u-size6 u-spacer-base">
+              <ProductCard />
+            </div>
+          )}
+        </div>
+      </div>
     );
   }
 }
