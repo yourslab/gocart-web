dacfe4c48d44ffa23d8deecf6a8407c80ce911d3
srph <seraphipod@gmail.com>
2016-06-06 19:18:56 +0800

    Post.Archive: Add "delete" button

--- src/views/app.manage-posts.archived/index.jsx
+++ src/views/app.manage-posts.archived/index.jsx
@@ -8,6 +8,16 @@ export default class AppManagePostsArchivedView extends Component {
       <div>
         <Helmet title="Archived Posts" />
 
+        <div className="SidebarContainer-panelHeading">
+          <div className="SidebarContainer-panelHeadingSection" />
+
+          <div className="SidebarContainer-panelHeadingSection">
+            <button className="Btn Btn--primary Btn--inverted Btn--borderless">
+              Delete all permanently
+            </button>
+          </div>
+        </div>
+
         <div className="Grid">
           {[[], [], [], [], [], []].map(() =>
             <div className="Grid-cell u-size6 u-spacer-base">
