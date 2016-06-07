57e7ee8aa345df1ca1f0e8b16497f28bb80062ff
srph <seraphipod@gmail.com>
2016-06-08 01:41:16 +0800

    Add placeholder for other pages

--- src/views/app.me/index.jsx
+++ src/views/app.me/index.jsx
@@ -7,9 +7,12 @@ export default class AppMeView extends Component {
       <div className="Container">
         <div className="SidebarContainer">
           <div className="Sidebar">
-            <IndexLink to="/me" className="Sidebar-item" activeClassName="Sidebar-item--active">Profile Settings</IndexLink>
+            <IndexLink to="/me" className="Sidebar-item" activeClassName="Sidebar-item--active">Profile</IndexLink>
             <Link to="/me/password" className="Sidebar-item" activeClassName="Sidebar-item--active">Change Password</Link>
+            <a href="#" className="Sidebar-item">Billing</a>
+            <a href="#" className="Sidebar-item">Orders</a>
             <Link to="/me/settings" className="Sidebar-item" activeClassName="Sidebar-item--active">Privacy</Link>
+            <a href="#" className="Sidebar-item">Profile</a>
           </div>
 
           <div className="SidebarContainer-panel">
