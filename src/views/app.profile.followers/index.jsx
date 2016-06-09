1e4ded0ea20bfd5b0476bf5033d822333de25f87
maebe22 <maeby1115@gmail.com>
2016-06-09 12:57:23 +0800

    change library from react-skylight to react-modal

--- src/views/app.profile.followers/index.jsx
+++ src/views/app.profile.followers/index.jsx
@@ -1,24 +1,45 @@
 import React, {Component} from 'react';
-import Skylight from 'react-skylight';
 import {Link} from 'react-router';
-import history from 'app/history';
+import Modal from 'react-modal';
+
+const dialogStyle = {
+	content: {
+		width: '35%',
+		height: 460,
+		overflow: 'visible',
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		marginTop: 100,
+		boxShadow: '0px 0px 4px rgba(0, 0, 0, .14), 0px 4px 8px rgba(0, 0, 0, .28)',
+		zIndex: 100
+	},
+
+	overlay: {
+		zIndex: 99,
+		backgroundColor: 'rgba(0, 0, 0, 0.3)'
+	}
+};
 
 export default class AppProfileFollowersView extends Component {
+	state = {
+		open: false
+	}
+
 	componentDidMount() {
-		this.refs.dialogue.show();
+		this.setState({ open: true });
 	}
 
 	componentWillUnmount() {
-		this.refs.dialogue.hide();
+		this.setState({ open: false });
 	}
 
 	render() {
 		return (
-			<Skylight
-			 	ref="dialogue"
-			 	dialogStyles={{ height: 'auto', width: '35%', left: '55%' }}
-			 	afterClose={() => history.push('/@srph')}>
-			 	<div className="ModalListPanel">
+			<Modal
+				isOpen={this.state.open}
+				onRequestClose={this.componentWillUnmount}
+				style={dialogStyle}>
+				<div className="ModalListPanel">
 			 		<div className="ModalListPanel-heading"> 
 			 			<h1> Followers </h1>  
 			 		</div>
@@ -62,12 +83,16 @@ export default class AppProfileFollowersView extends Component {
 			 		</div>
 
 			 		<div className="ModalListPanel-actions">
-			 			<button className="Btn Btn--info Btn--block">
+			 			<Link to="/@srph" className="Btn Btn--info Btn--block">
 			 				Done 
-			 			</button>
+			 			</Link>
 			 		</div>
 			 	</div>
-			</Skylight>
+			</Modal>
 		);
+	}
+
+	onCloseModal = () => {
+		this.setState({ open: false });
 	}
 }
