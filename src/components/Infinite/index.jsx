d2b20027628ee89712ca2a79fda6e53c8f5e1e8b
srph <seraphipod@gmail.com>
2016-06-11 03:42:43 +0800

    Feed: Init

---
+++ src/components/Infinite/index.jsx
@@ -0,0 +1,66 @@
+import React, {Component, PropTypes} from 'react';
+import {scrollTop, height} from 'dom-helpers';
+
+/**
+ * A basic and simple InfiniteScroll implementation
+ */
+export default class Infinite extends Component {
+  static propTypes = {
+    callback: PropTypes.func.isRequired,
+    offset: PropTypes.number,
+    disabled: PropTypes.bool,
+    container: PropTypes.bool
+  };
+
+  static defaultProps = {
+    offset: 200,
+    disabled: false,
+    container: false
+  };
+
+  // The scrolling container.
+  // If the container is window, this is set to window.
+  // Otherwise, set to the container element.
+  get scroller() {
+    // return this.props.container
+    //   ? this.refs.
+    //   : window;
+    return window;
+  }
+
+  // The container.
+  // If the container is window, this is set to body.
+  // Otherwise, set to the container element.
+  get container() {
+    return document.querySelector('html, body');
+  }
+
+  componentDidMount() {
+    this.scroller.addEventListener('scroll', this.handle);
+  }
+
+  componentWillUnmount() {
+    this.scroller.removeEventListener('scroll', this.handle);
+  }
+
+  render() {
+    return <div ref="container">{this.props.children}</div>;
+  }
+
+  handle = () => {
+    const {container, scroller} = this;
+    const {disabled, offset, callback} = this.props;
+
+    if ( this.props.disabled ) {
+      return;
+    }
+
+    const totalScroll = scrollTop(scroller);
+    const containerBottom = container.scrollHeight;
+    const scrollerHeight = height(scroller);
+
+    if ( totalScroll + offset >= containerBottom - scrollerHeight ) {
+      callback();
+    }
+  }
+}
