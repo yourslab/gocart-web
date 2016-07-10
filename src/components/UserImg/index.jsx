971996a1fd77c42a2082fb560f264cca4002e5de
srph <seraphipod@gmail.com>
2016-07-10 12:56:28 +0800

    Avatar: Fall back to default
    Resolve #42

--- src/components/UserImg/index.jsx
+++ src/components/UserImg/index.jsx
@@ -1,8 +1,11 @@
 import React, {PropTypes} from 'react';
 import getUserImg from 'app/utils/getUserImg';
+import StaticImg from 'app/components/StaticImg';
 
 const UserImg = ({src, username, ...props}) =>
-  <img src={getUserImg(username, src)} {...props} />
+  src == null
+    ? <StaticImg src="avatar.svg" {...props} />
+    : <img src={getUserImg(username, src)} {...props} />
 
 UserImg.propTypes = {
   username: PropTypes.string.isRequired
