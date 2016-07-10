99c243d45143e7dce1bc68a140cb1aaeef6939af
maebe22 <maeby1115@gmail.com>
2016-07-10 22:04:12 +0800

    remove StaticImg

--- src/views/app.me.home/components/PhotoUpload.jsx
+++ src/views/app.me.home/components/PhotoUpload.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import map from 'lodash/map';
 import reader from 'app/utils/filereader';
-import StaticImg from 'app/components/StaticImg';
 import UserImg from 'app/components/UserImg';
 
 export default class PhotoUpload extends Component {
@@ -12,10 +11,7 @@ export default class PhotoUpload extends Component {
 
         <div className="AvatarUploadWidget-inner">
           <button type="button" className="PlainBtn" onClick={this.handleAdd}>
-            {this.props.photo && this.props.photo.length
-              ? <UserImg src={this.props.photo} username={this.props.username} className="AvatarUploadWidget-avatar" alt={`${this.props.username}'s Avatar`} />
-              : <StaticImg src="avatar.svg" className="AvatarUploadWidget-avatar" alt="Avatar" />
-            }
+            <UserImg src={this.props.photo} username={this.props.username} className="AvatarUploadWidget-avatar" alt={`${this.props.username}'s Avatar`} />
           </button>
 
           <div>
